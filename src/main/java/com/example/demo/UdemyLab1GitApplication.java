8f06ff5b2b74060531542fc192f7c18f9c00477a
Edson Contreras <edrecon@gmail.com>
2017-09-29 14:37:38 -0300

    Basic files for Lab 1 project.

---
+++ src/main/java/com/example/demo/UdemyLab1GitApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyLab1GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyLab1GitApplication.class, args);
+	}
+}
